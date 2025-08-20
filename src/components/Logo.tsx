c55ab8be378769d82356aa5025864c42ac7e33d8
Ban68 <carlosvives89@hotmail.com>
2025-08-20 17:28:15 +0200

    Logos

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,9 @@
+import Image from 'next/image';
+
 export function Logo() {
   return (
     <div className="flex h-8 w-8 items-center justify-center rounded-full bg-lp-primary-1 p-1">
-      <span className="font-colette text-lg font-bold text-lp-primary-2">
-        LP
-      </span>
+      <Image src="/placeholder-logo.png" alt="LePrêt Capital Logo" width={32} height={32} />
     </div>
   );
 }
