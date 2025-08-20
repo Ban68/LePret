ac57de33a2da7e271a8e628e1a92d7c6d69acd0e
Ban68 <carlosvives89@hotmail.com>
2025-08-20 20:58:58 +0200

    Image Size 50%

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -21,7 +21,7 @@ export function Hero() {
           <Link href="/contacto">Hablar con un asesor <span aria-hidden="true">→</span></Link>
         </Button>
       </div>
-      <div className="mt-16 flow-root">
+      <div className="mt-16 flow-root w-1/2 mx-auto">
         <div className="rounded-2xl bg-lp-sec-4 p-2 ring-1 ring-inset ring-lp-sec-1/10 lg:p-4">
           <div className="bg-white rounded-xl shadow-2xl ring-1 ring-gray-900/10 overflow-hidden">
             <Image
