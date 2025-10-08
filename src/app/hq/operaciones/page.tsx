d90de75d2a3ca5c1d944686dd80101efa904d876
Ban68 <130825780+Ban68@users.noreply.github.com>
2025-10-08 16:56:34 +0200

    Separate KYC queue from operations

--- src/app/hq/operaciones/page.tsx
+++ src/app/hq/operaciones/page.tsx
@@ -1,10 +1,8 @@
-import { KycQueue } from "../ui/KycQueue";
 import { RequestsBoard } from "../ui/RequestsBoard";
 
 export default function HqOperationsPage() {
   return (
     <div className="space-y-8">
-      <KycQueue />
       <RequestsBoard />
     </div>
   );
